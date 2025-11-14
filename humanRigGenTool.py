d2875eeef4f8d484ae3e1760e75adae48670847f
Evie <eyg230000@utdallas.edu>
2025-11-14 13:12:12 -0600

    create blank window

--- humanRigGenTool.py
+++ humanRigGenTool.py
@@ -7,6 +7,7 @@ import maya.cmds as cmds
 
 # Boilerplate code, just copy and keep handy somewhere.
 # import riggen
+# import importlib
 # importlib.reload(riggen)
 
 # win = riggen.RigGenWin()
@@ -17,13 +18,19 @@ def get_maya_main_win():
     return wrapInstance(int(main_win), QtWidgets.QWidget)
 
 class RigGenWin(QtWidgets.QDialog):
-    pass
+    def __init__(self):
+        # runs the init code of the parent QDialog class
+        super().__init__(parent=get_maya_main_win())
+        self.rigGen = RigGen()
+        self.setWindowTitle("Simple Humanoid Rig Generator")
+        self.resize(250, 500)
+
     #Checkbox for fk, ik, or both with a switch.
     #Button for standard humanoid rig generation.
     #Buttons to create unique curves, not rigged.
+    pass
 
 class RigGen():
-    pass
     # Make unique controls
     # Make IK rig functional
     # Make FK rig functional
@@ -31,3 +38,4 @@ class RigGen():
     # Make a spline rig functional
     # Make controls change color based on L/R naming
     # Make controls change boldness based on the control's function
+    pass
